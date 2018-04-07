762060dfc9cd335a32c464440b99ce8ffe2ae9b4
Andrew Bass <andybass009@gmail.com>
2018-04-07 18:32:46 -0400

    Add database support

--- strategy.py
+++ strategy.py
@@ -3,12 +3,13 @@ import flask as fl
 from flask import Flask
 
 from flask_socketio import SocketIO
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_envvar("STRATEGY_CFG")
 
 sio = SocketIO(app)
+db = SQLAlchemy(app)
 
 @app.route("/")
 def test():
