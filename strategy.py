5ae1b2e1ef56e2873eb37b715baa0339618947f2
Andrew Bass <andybass009@gmail.com>
2018-04-07 18:31:42 -0400

    Add config file support

--- strategy.py
+++ strategy.py
@@ -2,7 +2,13 @@
 import flask as fl
 from flask import Flask
 
+from flask_socketio import SocketIO
+
+
 app = Flask(__name__)
+app.config.from_envvar("STRATEGY_CFG")
+
+sio = SocketIO(app)
 
 @app.route("/")
 def test():
