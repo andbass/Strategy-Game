5ac42cf0181e7b6b192eedb22bc6398cfb1a0bf6
Andrew Bass <andybass009@gmail.com>
2018-04-13 20:48:32 -0400

    Add modal create game dialog

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+
+function onCreateGame() {
+
+}
+
+$(document).ready(function() {
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
+    socket.on("connect", function() {
+        socket.emit("message", { data: "Dyalnana" })
+    })
+})
