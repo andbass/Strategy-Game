80e2cd153223b97d9e2a4d4e2022e5980e38b999
root <root@Ophiuchus.localdomain>
2018-04-16 13:36:52 -0400

    canvas function cleaning and attack interface

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,30 +1,51 @@
 class testjson {
     constructor(mapData, playerData) {
-        this.mapData = mapData;
-        this.playerData = playerData;
+        this.mapData = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0], 
+            [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0], 
+            [0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0], 
+            [0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0],
+            [0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0], 
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+
+        this.playerData = [
+            new testplayer("Bob", 1, 1, 4, "#ff0000", 0),
+            new testplayer("Cob", 5, 4, 5, "#00ffff", 1),
+            new testplayer("Dob", 3, 3, 5, "#0000ff", 2)
+        ];
     }
 }
 class testplayer {
-    constructor(posX, posY, move, color, ranged) {
+    constructor(name, posX, posY, move, color, ranged) {
         this.x = posX;
         this.y = posY;
+        this.name = name;
         this.canMove = 1;
         this.canAttack = 1;
         this.color = color;
         this.baseColor = color;
-        this.skills = ["attack", "double attack", "poke"];
+        this.skills = ["Attack", "Quick Attack", "Poke"];
+        this.hp = 20;
+        this.str = 10;
+        this.def = 5;
         switch (ranged) {
             case 0:
                 this.attackRange = [
                     [ 0, 1, 0],
                     [ 1, 0, 1],
                     [ 0, 1, 0]];
+                this.def = 10;
                 break;
             case 1:
                 this.attackRange = [
                     [1, 1, 1],
                     [1, 0, 1],
                     [1, 1, 1]];
+                this.str = 15;
                 break;
             case 2:
                 this.attackRange = [
@@ -45,37 +66,28 @@ class testplayer {
             [ 0, 0, 0, 1, 0, 0, 0]];
     }
 }
+
+// all of the above should be grabbed from other files
+
 class Menu {
     constructor(id, posX, posY, options) {
         this.x = posX;
         this.y = posY;
         this.id = id;
-        this.width = 70;
         this.options = options;
         this.marginX = 4;
         this.marginY = 14;
         this.height = this.marginY * (options.length + 0.5);
+        var length = 0;
+        for (var i = 0; i < options.length; i++) {
+            if (options[i].length > length)
+                length = options[i].length;
+        }
+        this.width = (length + 1) * (this.marginX + 1);
+        if (this.width < 70)
+            this.width = 70;
     }
-
 }
-
-var mapData = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 0, 0, 1, 1, 1, 0], 
-    [0, 1, 1, 1, 0, 0, 0, 0, 0, 0], 
-    [0, 0, 1, 0, 0, 0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 1, 0, 0, 1, 0, 1, 0], 
-    [0, 0, 1, 0, 0, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 0, 1, 0, 1, 0], 
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
-
-var playerData = [
-    new testplayer(1, 1, 4, "#ff0000", 0),
-    new testplayer(5, 4, 5, "#00ffff", 1),
-    new testplayer(3, 3, 5, "#0000ff", 2)
-    ];
 
 var canvas = null;
 var ctx = null;
@@ -86,18 +98,20 @@ var tileHeight = 20;
 var mouseX;
 var mouseY;
 
+var map = [];
+var menus = [];
 var units = [];
+
 var activeUnit = -1;
 var targetUnit = -1;
-
-var menus = [];
+var attack = -1;
 
 window.onload = function() {
 
-    var data = new testjson(mapData, playerData); 
-    menus = [
-   //     new Menu(-1, 50, 50, ["nothing", "blank"])
-    ];
+    var data = new testjson(); 
+
+    units = data.playerData;
+    map = data.mapData;
 
     canvas = document.getElementById('main-canvas');
     canvas.width = canvas.offsetWidth;
@@ -144,15 +158,19 @@ function handleMenu(menu) {
             break;
         // action choice menu
         case 1:
+            menus.splice(menu+1, menus.length-menu-1);
             handleAttack(index);
+            break;
+        case 2:
+            handleConfirm();
             break;
     }
 }
 
 function handleNextTurn() {
     console.log("next turn");
-    for (var i = 0; i < playerData.length; i++) {
-        var p = playerData[i];
+    for (var i = 0; i < units.length; i++) {
+        var p = units[i];
         p.canMove = 1;
         p.canAttack = 1;
     }
@@ -160,31 +178,61 @@ function handleNextTurn() {
 }
 
 function handleAttack(index) {
-    var p = playerData[activeUnit];
-    console.log("attack selected: " + p.skills[index]);
-    p.canAttack = 0;
+    var p = units[activeUnit];
+    attack = index;
+    displayAttackStats();
+}
+
+// stat comparison
+function displayAttackStats() {
+    var xPos = menus[menus.length-1].x + menus[menus.length-1].width;
+    var yPos = menus[menus.length-1].y;
+    var p = units[activeUnit];
+    var t = units[targetUnit];
+    var player_text = [p.name + ": " + p.skills[attack], "  HP: " + p.hp, "  str: " + p.str, "  def: " + p.def];
+    var target_text = [t.name + ": Counter", "  HP: " + t.hp, "  str: " + t.str, "  def: " + t.def];
+    menus[menus.length] = new Menu(-1, xPos, yPos, player_text);
+    menus[menus.length] = new Menu(-1, xPos + menus[menus.length-1].width, yPos, target_text);
+    menus[menus.length] = new Menu(2, xPos, yPos + menus[menus.length-1].height, ["Confirm"]);
+    drawState();    
+}
+
+// actually use selected action
+function handleConfirm() {
+    var p = units[activeUnit];
+    var t = units[targetUnit];
+    console.log("attack selected: " + p.skills[attack]);
     p.color = p.baseColor;
+    p.canAttack = 0;
+    // p.canMove = 0;   // adding this will prevent out of order actions, but its kinda fun to leave in, (maybe wrap it into a skill)
     activeUnit = -1;
+    var damage = p.str - t.def;
+    if (damage < 1)
+        damage = 1;
+    t.hp -= damage;
+    if (t.hp <= 0)
+        units.splice(targetUnit, 1);
     closeMenus();
+    drawState();
 }
 
 function handleAction() {
     var tileX = Math.floor(mouseX/tileWidth);
     var tileY = Math.floor(mouseY/tileHeight);
-    if (tileY >= mapData.length || tileX >= mapData[0].length)
+    if (tileY >= map.length || tileX >= map[0].length)
         return;
 
     targetUnit = -1;
 
-    for (var i = 0; i < playerData.length; i++) {
-        var p = playerData[i];
+    for (var i = 0; i < units.length; i++) {
+        var p = units[i];
         if (tileX == p.x && tileY == p.y) {
             targetUnit = i;
             break;
         }
     }
 
-    var p = playerData[activeUnit];
+    var p = units[activeUnit];
 
     if (targetUnit == -1) {
         if (activeUnit == -1) {
@@ -198,30 +246,35 @@ function handleAction() {
                 p.x = tileX;
                 p.y = tileY;
                 p.canMove = 0;
+            } else {
+                activeUnit = -1;
+                p.color = p.baseColor;
             }
-            activeUnit = -1;
-            p.color = p.baseColor
+            closeMenus();
         }
     } else {
         closeMenus();
         if (activeUnit == -1) {
             activeUnit = targetUnit;
-            p = playerData[activeUnit];
+            p = units[activeUnit];
             p.color = "#00ff00";
         } else {
             if (canReach(p, p.attackRange, tileX, tileY) && p.canAttack) {
                 menus[menus.length] = new Menu(1, mouseX, mouseY, p.skills); 
-                p.canAttack = 0;
+            } else {
+                activeUnit = -1;
+                p.color = p.baseColor;
             }
         }
     }
     drawState();
     if (activeUnit != -1) {
         if (p.canMove)
-            drawMove(playerData[activeUnit]);
+            drawMove(units[activeUnit]);
         if (p.canAttack)
-            drawAttack(playerData[activeUnit]);
+            drawAttack(units[activeUnit]);
     }
+    drawMenus();
 }
 
 function drawState() {
@@ -229,9 +282,12 @@ function drawState() {
         return;
     }
 
-    for (var x = 0; x < mapData.length; x++) {
-        for (var y = 0; y < mapData[x].length; y++) {
-            switch(mapData[y][x]) {
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    for (var y = 0; y < map.length; y++) {
+        for (var x = 0; x < map[y].length; x++) {
+            switch(map[y][x]) {
                 // replace case statements with tile data based on Type enum
                 case 0:
                     ctx.fillStyle = "#000000";
@@ -322,8 +378,8 @@ function drawUnits() {
     if (ctx == null)   
         return;
 
-    for (var i = 0; i < playerData.length; i++) {
-        var p = playerData[i];
+    for (var i = 0; i < units.length; i++) {
+        var p = units[i];
         ctx.fillStyle = p.color;
         ctx.fillRect(p.x*tileWidth + 5, p.y*tileHeight + 5, tileWidth - 10, tileHeight - 10);
     }
